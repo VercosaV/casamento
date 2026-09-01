fa5aee29c8f87be5762973ac26f4eea12498ab81
victor vercosa <victorhvercosag@gmail.com>
2026-08-31 23:34:19 -0300

    começando a tela principal

--- resources/views/noivos/index.blade.php
+++ resources/views/noivos/index.blade.php
@@ -3,40 +3,18 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Categorias</title>
+<title>Victor e Kamilly</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
  <div class="container py-3"> 
-          @if (session('mensagem'))
-            <p>{{ session('mensagem') }}</p>
-          @endif
+          
+
+          <h1>Victor </br> e </br> Kamilly</h1>
+
           
           <h2>Noivos</h2>
-          <a href="/noivos/create" class="btn btn-success mb-3">Novo Registro</a>
-          <table class="table table-hover table-striped">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Descrição</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach($noivos as $noivo)
-                <tr>
-                    <td>{{ $noivo->id }}</td>
-                    <td>{{ $noivo->nome }}</td>
-                    <td>{{ $noivo->descricao }}</td>
-                    <td class="d-flex gap-2">
-                        <a href="/noivos/{{ $noivo->id }}/edit" class="btn btn-sm btn-warning">Editar</a>
-                        <a href="/noivos/{{ $noivo->id }}" class="btn btn-sm btn-info">Consultar</a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-          </table>
+          
         
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
