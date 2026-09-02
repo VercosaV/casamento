e4be5e1fb8cf62bd11186ea8096861a943e95e43
VercosaV <victorhvercosag@gmail.com>
2026-09-02 20:57:46 -0300

    oaoao

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\PresenteController;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
